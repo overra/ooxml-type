7cbd2151a9ea244e2e8f4e16c8d2b61246a6cdab
Adam Snodgrass <overra@gmail.com>
2016-03-18 23:13:40 -0500

    Unzip and parse ooxml buffer

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var Zip = require('adm-zip');
+
+var XLSX = /PartName\=\"\/xl/;
+var DOCX = /PartName\=\"\/word/;
+var PPTX = /PartName\=\"\/ppt/;
+
+module.exports = function(buffer) {
+  var zip = new Zip(buffer);
+  var entries = zip.getEntries();
+  var type;
+
+  for (var i = 0; i < entries.length; i++) {
+    var entry = entries[i];
+    if (entry.name === '[Content_Types].xml') {
+      var data = entry.getData().toString();
+
+      if (XLSX.test(data)) {
+        return 'xlsx';
+      } else if (DOCX.test(data)) {
+        return 'docx';
+      } else if (PPTX.test(data)) {
+        return 'pptx';
+      }
+    }
+  }
+}
